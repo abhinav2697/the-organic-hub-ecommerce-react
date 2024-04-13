15870f7aa0249a35d2fb753d4b5553613e0db185
abhinav2697 <abhinav2697@gmail.com>
2024-04-13 14:20:39 +0530

    new files and features added

---
+++ happyfitecommerce/src/Components/Filter/SortByPrice.jsx
@@ -0,0 +1,48 @@
+import { useFilter } from '../../context/filter-product-context';
+
+export const SortByPrice = () => {
+  const {
+    state: { sortBy },
+    productDispatch
+  } = useFilter();
+
+  return (
+    <div className="sort-container">
+      <h3 className="sub-title margin16-top-down">Sort by</h3>
+      <div className="margin8-top-down">
+        <label className="men d-flex gap align-center">
+          <input
+            className="check-box"
+            type="radio"
+            name="sortByPrice"
+            value="lowToHigh"
+            checked={sortBy === 'LOW_TO_HIGH'}
+            onChange={() => {
+              productDispatch({
+                type: 'LOW_TO_HIGH'
+              });
+            }}
+          />
+          Price - Low to High
+        </label>
+      </div>
+      <div className="margin8-top-down">
+        <label className="men d-flex gap align-center">
+          <input
+            className="check-box"
+            type="radio"
+            name="sortByPrice"
+            value="highToLow"
+            checked={sortBy === 'HIGH_TO_LOW'}
+            onChange={() => {
+              productDispatch({
+                type: 'HIGH_TO_LOW'
+              });
+            }}
+          />
+          Price - High to Low
+        </label>
+      </div>
+    </div>
+  );
+};
