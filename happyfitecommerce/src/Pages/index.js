8e0bfb8897850ee4c6bff86a0a6218e6ae1f6708
abhinav2697 <abhinav2697@gmail.com>
2024-04-15 00:06:07 +0530

    added 404 error page

--- happyfitecommerce/src/Pages/index.js
+++ happyfitecommerce/src/Pages/index.js
@@ -1,6 +1,7 @@
 export { Home } from "./Home";
 export { Wishlist } from "./Wishlist";
 export { Cart } from "./Cart";
+export { Error } from "./404/404";
 export { Address } from "./Address";
 export { CheckoutPage } from "./CheckoutPage";
 export { Order } from "./Order";
