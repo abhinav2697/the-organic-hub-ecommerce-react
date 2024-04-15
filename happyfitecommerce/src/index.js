1b936f25ec323167af29df54cd791f1ed4781045
abhinav2697 <abhinav2697@gmail.com>
2024-04-15 16:45:48 +0530

    added server

--- happyfitecommerce/src/index.js
+++ happyfitecommerce/src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { makeServer } from "./server";
+import { FilterProvider,CartProvider,AuthProvider, AddressProvider, } from './context';
+import { Address } from './Pages';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <FilterProvider>
+        <CartProvider>
+          <AuthProvider>
+            <AddressProvider>
+              <App />
+              </AddressProvider>
+            </AuthProvider>
+          </CartProvider>
+        </FilterProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
+reportWebVitals();
